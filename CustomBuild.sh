84b96b48ddb6a9276eef3629501f0a8e17b9b42c
Prikith <38623521+Prikith@users.noreply.github.com>
2019-02-02 01:11:21 +0100

    Update CustomBuild.sh

--- CustomBuild.sh
+++ CustomBuild.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-mv ralim_custom ralim_custom.old
+_BuildNumber=01
+_BuildNumber=$1
+TIMESTAMP=`date "+%Y%m%d-%H%M%S"`
+mv ralim_custom ./Workspace/Old/Custom/ralim_custom-$TIMESTAMP
 cd ralim_git
 git pull
 cd ..
 cp -r ralim_git ralim_custom
-cp ./ralim_changedfiles/build.sh ./ralim_custom/workspace/TS100/build.sh
-cp -rf ./ralim_changedfiles/Translations/*.json ./ralim_custom/"Translation Editor"/
+cp ./Workspace/BuildScripts/Custom/build$_BuildNumber.sh ./ralim_custom/workspace/TS100/build.sh
+cp -rf ./Workspace/Translations/*.json ./ralim_custom/"Translation Editor"/
 cd ralim_custom/workspace/TS100
 chmod +x build.sh
 ./build.sh
